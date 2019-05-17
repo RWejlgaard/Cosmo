b7282ba0b5e34d22692fc34cc47caab3ad7c4f18
Rasmus Wejlgaard <wejlgaard@live.dk>
2019-05-17 22:46:52 +0700

    Initial Commit

---
+++ Cosmo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cosmo
+//
+//  Created by Rasmus Wejlgaard on 17/05/2019.
+//  Copyright © 2019 Rasmus Wejlgaard. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
