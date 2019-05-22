b2f5044ffc20ec472f03ebc1ff180613d635d66e
Rasmus Wejlgaard <wejlgaard@live.dk>
2019-05-23 04:43:10 +0700

    removed moved files

--- Cosmo/libs/TextToSpeech.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TextToSpeech.swift
-//  Cosmo
-//
-//  Created by Rasmus Wejlgaard on 18/05/2019.
-//  Copyright © 2019 Rasmus Wejlgaard. All rights reserved.
-//
-
-import Foundation
--- Cosmo/router.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Router.swift
-//  Cosmo
-//
-//  Created by Rasmus Wejlgaard on 18/05/2019.
-//  Copyright © 2019 Rasmus Wejlgaard. All rights reserved.
-//
-
-import Foundation
